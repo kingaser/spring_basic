483e27d65e7a02593374c08c30247f6fe6f54f4e
kingaser <kignaser@naver.com>
2023-01-28 21:32:15 +0900

    Fead : AutoAppConfig 추가 ComponentScan 사용

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -14,9 +14,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
-    //@Bean memberService -> new MemoryMemberRepository()
-    //@Bean orderService -> new MemoryMemberRepository()
-
     @Bean
     public MemberService memberService() {
         System.out.println("call AppConfig.memberService");
