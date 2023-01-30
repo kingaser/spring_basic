da9c1d5f66d8bc7c9e238de32927fe48ee1e40ea
kingaser <kignaser@naver.com>
2023-01-30 17:34:02 +0900

    Feat : request scope 추가, 스프링 핵심 원리 - 기본편 완료

---
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,55 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) throws InterruptedException {
+        String requestsURL = request.getRequestURI().toString();
+
+        System.out.println("myLogger = " + myLogger.getClass());
+        myLogger.setRequestURL(requestsURL);
+
+        myLogger.log("controller test");
+        Thread.sleep(1000);
+        logDemoService.logic("testId");
+
+        return "OK";
+    }
+}
+
+//@Controller
+//@RequiredArgsConstructor
+//public class LogDemoController {
+//
+//    private final LogDemoService logDemoService;
+//    private final ObjectProvider<MyLogger> myLoggerProvider;
+//
+//    @RequestMapping("log-demo")
+//    @ResponseBody
+//    public String logDemo(HttpServletRequest request) throws InterruptedException {
+//        String requestsURL = request.getRequestURI().toString();
+//        MyLogger myLogger = myLoggerProvider.getObject();
+//        myLogger.setRequestURL(requestsURL);
+//
+//        myLogger.log("controller test");
+//        Thread.sleep(1000);
+//        logDemoService.logic("testId");
+//
+//        return "OK";
+//    }
+//}
